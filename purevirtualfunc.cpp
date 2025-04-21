1ccdecfca306691d8b57218426e9607b44b85a7b
Akanksha-0120 <150513324+Akanksha-0120@users.noreply.github.com>
2025-04-21 17:07:40 +0530

    Add files via upload

---
+++ purevirtualfunc.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+    int x;
+    public:
+       virtual void fun()=0;
+       int getX(int x)
+       {
+         return x;
+       }
+};
+class Derived:public Base{
+    int y;
+    public:
+        void fun()
+        {
+            cout<<"fun() called";
+        }
+};
+int main()
+{
+    Base *b;
+    //b.getX();
+   // b.fun();
+    Derived d;
+    d.fun();
+   return 0;
+}
